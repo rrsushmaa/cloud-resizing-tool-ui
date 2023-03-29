4589afd2453a5f5b91cb37277e4bfeef2613fd14
Sushmaa R R <sushmaa.rr@thoughtworks.com>
2023-03-29 18:27:06 +0530

    Antony/Sushmaa - Add table into dashbard component

--- src/api/dashboard.ts
+++ src/api/dashboard.ts
@@ -2,5 +2,12 @@ import {apiClient} from "./api";
 
 export const getDashboard = async () => {
     const response = await apiClient.get('/src/assets/data.json');
+    await sleep()
     return response.data
+}
+
+export const sleep = () => {
+    return new Promise(resolve => {
+        setTimeout(resolve, 2000)
+    })
 }
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,13 +1,27 @@
-import {FC} from "react";
+import {FC, useMemo} from "react";
 import {useQuery} from "@tanstack/react-query";
 import {getDashboard} from "../api/dashboard";
+import {DataGrid, GridColDef, GridRowsProp} from "@mui/x-data-grid";
 
+
+const columns: GridColDef[] = [
+    { field: 'resource', headerName: 'Resource', width: 150 },
+    { field: 'type', headerName: 'Type', width: 150 },
+    { field: 'averageCpuUsagePercentage', headerName: 'Average CPU %', width: 150 },
+    { field: 'averageMemoryUsagePercentage', headerName: 'Average memory %', width: 150 },
+    { field: 'costPerMonth', headerName: 'Cost', width: 150 },
+    { field: 'suggestedScheme', headerName: 'Suggestion', width: 150 },
+    { field: 'optimize', headerName: 'Optimize', width: 150 },
+];
 export const Dashboard : FC = () => {
     const { data } = useQuery({ queryKey: ['dashboard'], queryFn: getDashboard })
+    const rows = useMemo(()=> data?.data|| [], [data])
     return (
         <div>
-            <p>Helloooo</p>
-            <p>{JSON.stringify(data)}</p>
+
+            <div style={{ height: 300, width: '100%' }}>
+                <DataGrid rows={rows} columns={columns} />
+            </div>
         </div>
     )
 }
